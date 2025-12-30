9b7c4d6bf1e3cdc5cb0b7f4b288a7f79af8422dc
ranjeetsingh <ranjeetsingh0611@gmail.com>
2025-12-30 15:34:17 +0530

    Add memory management functionality to chatbot and refactor conversation handling

--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -2,6 +2,7 @@ import uuid
 
 from langchain_core.prompts import PromptTemplate
 
+from src.memory.memory_decider import MemoryDecider
 from src.memory.short_term_memory import ShortTermMemory
 from src.ollama.ollama import Ollama
 
@@ -11,12 +12,19 @@ class ChatBot:
     def get_session_id(self):
         return str(uuid.uuid4())
 
+    def get_conversation(self, user_input, output):
+        messages = [{"role": "user", "content": user_input}, {"role": "assistant", "content": output}]
+        conversation = "\n".join(
+            f"{m['role']}: {m['content']}" for m in messages
+        )
+        return conversation
+
     def chat(self):
         try:
             ollama_model = Ollama()
-            chatbot = ChatBot()
-            session_id = chatbot.get_session_id()
+            session_id = self.get_session_id()
             short_term_memory = ShortTermMemory()
+            memory_decider = MemoryDecider()
             while True:
                 user_input = input("You:> ")
                 if "exit" in user_input:
@@ -44,8 +52,9 @@ class ChatBot:
 
                 chain = prompt | llm
                 response = chain.invoke({"user_input": user_input, "short_term_history": short_term_history_text})
-                short_term_memory.save_into_memory(session_id=session_id, user_input=user_input,
-                                                   output=response.content)
+                conversation = self.get_conversation(user_input, response.content)
+                memory_decider.save_to_memory(llm=llm, session_id=session_id, user_input=user_input,
+                                              output=response.content, user_conversation=conversation)
                 print("ChatBot:>", response.content)
         except Exception as e:
             print(f"Error during chat: {e}")
--- src/memory/memory_decider.py
+++ src/memory/memory_decider.py
@@ -1,21 +1,17 @@
 from langchain_classic.prompts import ChatPromptTemplate
-from src.memory.long_term_memory import LongTermMemory
 from src.memory.short_term_memory import ShortTermMemory
 
 
 class MemoryDecider(object):
-    def decide_memory(self, llm, user_input: str):
+    def save_to_memory(self, llm, session_id, user_input, output, user_conversation):
         try:
             prompt_template = ChatPromptTemplate([("system",
-                                                   "You're a helpful AI assistant.Decide which memory to use between short-term and long-term based on the user's input. Return only 'short-term' or 'long-term'. If you're not able to figure out just return short-term memory"),
-                                                  ("human", f"{user_input}")])
+                                                   "You're a helpful AI assistant. Decide whether to store the following user conversation in short-term or long-term memory or no need to store. If the information is already stored, do not store it again. Respond with only 'short-term', 'long-term', or 'None'."),
+                                                  ("human", f"{user_conversation}")])
             chain = prompt_template | llm
-            response = chain.invoke({"user_input": user_input})
-            print(response.content)
-            if "long-term" in response.content.lower():
-                return LongTermMemory()
-            else:
-                return ShortTermMemory()
+            response = chain.invoke({"user_input": user_conversation})
+            if "short-term" in response.content.lower():
+                short_term_memory = ShortTermMemory()
+                short_term_memory.save_into_memory(session_id=session_id, user_input=user_input, output=output)
         except Exception as e:
             print(f"Failed to decide memory: {e}")
-            return None
